93f41269a6c08e4f4ce52a798ff66f89b4befef1
Andrea Saccomanni <Andrea Saccomanni>
2024-10-24 10:15:42 +0200

    feedbakcp page

--- Assets/JavaScript/feedback.js
+++ Assets/JavaScript/feedback.js
@@ -1,108 +1,29 @@
+//per far illuminare la prima stella fissa
 window.onload = function () {
   const path = document.getElementById("star1");
-  path.removeAttribute("fill");
 };
-const arrayStar = document.querySelectorAll(".stars");
-arrayStar[0].onmouseenter = function () {
-  arrayStar[0].style.fill = "#00FFFF";
-};
-arrayStar[0].onmouseleave = function () {
-  arrayStar[0].style.fill = "#0B103B";
-};
+const arrayStar = document.querySelectorAll(".singleStars");
 
-for (let i = 1; i < arrayStar.length; i++) {
+//per illuminare le stelle passandoci sopra con il mouse
+
+for (let i = 0; i < arrayStar.length; i++) {
+  arrayStar[i].onclick = function () {
+    const highlighted = document.querySelectorAll(".highlighted");
+    highlighted.forEach((star) => star.classList.remove("highlighted"));
+    for (let j = 0; j <= i; j++) {
+      arrayStar[j].classList.add("highlighted");
+    }
+  };
+
   arrayStar[i].onmouseenter = function () {
     for (let j = 0; j <= i; j++) {
-      arrayStar[j].style.fill = "#00FFFF";
+      arrayStar[j].classList.add("select");
     }
-    arrayStar[i].onmouseleave = function () {
-      for (let j = 0; j <= i; j++) {
-        arrayStar[j].style.fill = "#0B103B";
-      }
-    };
   };
-  arrayStar[i].onclick = function () {
+
+  arrayStar[i].onmouseleave = function () {
     for (let j = 0; j <= i; j++) {
-      const path = arrayStar[j].queryselector(".stars");
-      path.removeAttribute("fill");
+      arrayStar[j].classList.remove("select");
     }
   };
 }
-
-/*arrayStar[1].onmouseenter = function () {
-  arrayStar[0].style.fill = "#00FFFF";
-  arrayStar[1].style.fill = "#00FFFF";
-};
-arrayStar[1].onmouseleave = function () {
-  arrayStar[0].style.fill = "#0B103B";
-  arrayStar[1].style.fill = "#0B103B";
-};
-
-arrayStar[2].onmouseenter = function () {
-  arrayStar[0].style.fill = "#00FFFF";
-  arrayStar[1].style.fill = "#00FFFF";
-  arrayStar[2].style.fill = "#00FFFF";
-};
-arrayStar[2].onmouseleave = function () {
-  arrayStar[0].style.fill = "#0B103B";
-  arrayStar[1].style.fill = "#0B103B";
-  arrayStar[2].style.fill = "#0B103B";
-};
-arrayStar[3].onmouseenter = function () {
-  arrayStar[0].style.fill = "#00FFFF";
-  arrayStar[1].style.fill = "#00FFFF";
-  arrayStar[2].style.fill = "#00FFFF";
-  arrayStar[3].style.fill = "#00FFFF";
-};
-arrayStar[3].onmouseleave = function () {
-  arrayStar[0].style.fill = "#0B103B";
-  arrayStar[1].style.fill = "#0B103B";
-  arrayStar[2].style.fill = "#0B103B";
-  arrayStar[3].style.fill = "#0B103B";
-};
-arrayStar[4].onmouseenter = function () {
-  arrayStar[0].style.fill = "#00FFFF";
-  arrayStar[1].style.fill = "#00FFFF";
-  arrayStar[2].style.fill = "#00FFFF";
-  arrayStar[3].style.fill = "#00FFFF";
-  arrayStar[4].style.fill = "#00FFFF";
-};
-arrayStar[4].onmouseleave = function () {
-  arrayStar[0].style.fill = "#0B103B";
-  arrayStar[1].style.fill = "#0B103B";
-  arrayStar[2].style.fill = "#0B103B";
-  arrayStar[3].style.fill = "#0B103B";
-  arrayStar[4].style.fill = "#0B103B";
-};
-arrayStar[1].onmouseenter = function () {
-  arrayStar[0].style.fill = "#00FFFF";
-  arrayStar[1].style.fill = "#00FFFF";
-};
-arrayStar[1].onmouseleave = function () {
-  arrayStar[0].style.fill = "#0B103B";
-  arrayStar[1].style.fill = "#0B103B";
-};
-arrayStar[1].onmouseenter = function () {
-  arrayStar[0].style.fill = "#00FFFF";
-  arrayStar[1].style.fill = "#00FFFF";
-};
-arrayStar[1].onmouseleave = function () {
-  arrayStar[0].style.fill = "#0B103B";
-  arrayStar[1].style.fill = "#0B103B";
-};
-arrayStar[1].onmouseenter = function () {
-  arrayStar[0].style.fill = "#00FFFF";
-  arrayStar[1].style.fill = "#00FFFF";
-};
-arrayStar[1].onmouseleave = function () {
-  arrayStar[0].style.fill = "#0B103B";
-  arrayStar[1].style.fill = "#0B103B";
-};
-arrayStar[1].onmouseenter = function () {
-  arrayStar[0].style.fill = "#00FFFF";
-  arrayStar[1].style.fill = "#00FFFF";
-};
-arrayStar[1].onmouseleave = function () {
-  arrayStar[0].style.fill = "#0B103B";
-  arrayStar[1].style.fill = "#0B103B";
-};*/
